228812b3d0baba78d7c4d2ebbd38aee58198398d
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-03-23 17:55:24 -0500

    fonts, started landing section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      stargaze: ["Stargaze"],
+      "stargaze-stencil": ["StargazeStencil"],
+      xxix: ["XXIX"],
+      "roboto-condensed": ["RobotoCondensed"],
+    },
+    colors: {
+      transparent: "transparent",
+      white: "white",
+      black: "black",
+      blue: "#333AE3",
+      purple: "#5D27E3",
+      grey: "#A3A3A3",
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".gradient-text": {
+          color: "transparent",
+          backgroundClip: "text",
+          backgroundImage: "linear-gradient(to right, #333AE3, #5D27E3)",
+        },
+      });
+    }),
+  ],
+};
