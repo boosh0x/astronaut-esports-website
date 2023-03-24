8243c354da51cfe9e8b3c429e5b875394e6ced7e
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-03-24 15:24:50 -0500

    Update Background.tsx

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,38 +1,19 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useEffect } from "react";
-import * as THREE from "three";
+import { Suspense } from "react";
 import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 export default function Background() {
   return (
     <div className="absolute top-0 left-0 w-full h-screen">
-      <Canvas
-
-      //   camera={{
-      //     fov: 60,
-      //     aspect: window.innerWidth / window.innerHeight,
-      //     near: 1,
-      //     far: 1000,
-      //     position: [0, 0, 1],
-      //     rotation: [Math.PI / 2, 0, 0],
-      //   }}
-      >
+      <Canvas>
         <mesh>
           <ambientLight intensity={0.3} />
           <pointLight position={[25, 25, 10]} />
-          <boxGeometry />
-          <meshStandardMaterial />
           <Suspense>
             <Quest />
           </Suspense>
         </mesh>
-        {/* <geometry
-    <vector3
-      x={Math.random() * 600 - 300}
-      y={Math.random() * 600 - 300}
-      z={Math.random() * 600 - 300}
-    /> */}
       </Canvas>
     </div>
   );
